79770365347ff1576696a482b003aff7ea0a723a
Juan Carlos Garcia <juancarlosgarcia_arg@hotmail.com>
2021-09-13 17:07:07 -0300

    VaultItemFactory class, removing not used classes.

--- propel-shared/models/vault.ts
+++ propel-shared/models/vault.ts
@@ -1,4 +1,9 @@
+import { PropelError } from "../core/propel-error";
 import { AuditedEntity } from "./audited-entity";
+import { Credential } from "./credential";
+import { CredentialTypes } from "./credential-types";
+import { WindowsVaultItem } from "./windows-vault-item";
+import { AWSVaultItem } from "./aws-vault-item";
 
 /**
  * Propel Secret Vault.
@@ -19,5 +24,35 @@ export class Vault<T> extends AuditedEntity {
     constructor(type: (new () => T)) {
         super();
         this.value = new type();
+    }
+}
+
+/**
+ * factory to create Vault Item instances starting from a credential.
+ */
+export class VaultItemFactory {
+
+    /**
+     * This method create a new Vault item instance of the right type based on the 
+     * credential type attribute.
+     * @param credential Credential that will own the Vault item.
+     * @returns a Vault item of the type specified by the credential.
+     */
+    static createFromCredential(credential: Credential): Vault<any> {
+
+        let ret: Vault<any>;
+
+        if (!credential) throw new PropelError(`We expect a valid Credential. the parameter "credential" is a null reference.`);
+        if (!credential.type) throw new PropelError(`The Credential instance doesn't have a valid CredentialType assigned."type" attribute value: "${String(credential.type)}".`);
+
+        switch (credential.type) {
+            case CredentialTypes.AWS:
+                ret = new Vault<AWSVaultItem>(AWSVaultItem);
+                break;
+            default:
+                ret = new Vault<WindowsVaultItem>(WindowsVaultItem);
+        }
+
+        return ret;
     }
 }
