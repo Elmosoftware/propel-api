0734788d4811627b577c8c0bcd46e6c199f2c3c9
Juan Carlos Garcia <juancarlosgarcia_arg@hotmail.com>
2022-04-15 10:03:45 -0300

    Fixing issue on PropelError when parsing stack.

--- propel-shared/core/propel-error.ts
+++ propel-shared/core/propel-error.ts
@@ -105,7 +105,7 @@ export class PropelError extends Error {
             ret = err.stack;
         }
         else {
-            ret = (Error().stack) ? String(Error().stack) : "";
+            ret = "";
         }
         return ret;
     }
