cc68d81f113036f2f4cc9ecea1029fbe1f7ac714
juancarlosgarcia-arg <juancarlosgarcia_arg@hotmail.com>
2022-11-09 16:54:39 -0300

    Cutover support files

---
+++ distrib/cutover/add-mongo-path.ps1
@@ -0,0 +1,10 @@
+﻿$currPath = [System.Environment]::GetEnvironmentVariable("Path")
+
+$currPath += ";C:\Program Files\MongoDB\Server\6.0\bin"
+
+[System.Environment]::SetEnvironmentVariable("Path", $currPath, [System.EnvironmentVariableTarget]::Machine)
+
+Write-Host "Changes done..."
+$currPath
+#Need to close the console to see the changes
+#[System.Environment]::GetEnvironmentVariable("Path")
