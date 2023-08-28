825e2404cabf189dba50814e53095bfcc2126372
juancarlosgarcia-arg <juancarlosgarcia_arg@hotmail.com>
2023-08-28 15:58:09 -0300

    Upgrading Angular Core and Cli to v15

--- propel-web/src/app/app-routing.module.ts
+++ propel-web/src/app/app-routing.module.ts
@@ -263,7 +263,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes, {})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
